566d97fa639324c055f2ffe137dda51b3e94d6e6
thyeone <thyeonee@gmail.com>
2025-08-16 02:05:14 +0900

    refactor: src 폴더로 이동

--- components/sheet/gorhom-sheet.tsx
+++
@@ -1,3 +0,0 @@
-import BottomSheet from '@gorhom/bottom-sheet'
-
-export { BottomSheet as GorhomSheet }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,11 @@
-import { zIndex } from './styles/zIndex'
+import { zIndex } from './src/styles/zIndex'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/app/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
