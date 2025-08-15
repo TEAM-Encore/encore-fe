9a04723e2f7a9ff3bac0d778c8abb60b7d4cb046
thyeone <thyeonee@gmail.com>
2025-08-16 02:23:13 +0900

    refactor: max-width 제거

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -21,9 +21,6 @@ module.exports = {
         extrabold: ['Pretendard-ExtraBold', 'sans-serif'],
         black: ['Pretendard-Black', 'sans-serif'],
       },
-      maxWidth: {
-        base: '500px',
-      },
     },
   },
   plugins: [],
