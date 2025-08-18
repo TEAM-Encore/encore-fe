2aefee022fa3f1e15db39a6d64ef0358362e7c70
thyeone <thyeonee@gmail.com>
2025-08-18 22:21:04 +0900

    fix: tailwind config fontsize 속성 수정

--- src/styles/fontSize.ts
+++ src/styles/fontSize.ts
@@ -1,4 +1,6 @@
-export const fontSize = {
+import type { ThemeConfig } from 'tailwindcss/types/config'
+
+export const fontSize: ThemeConfig['fontSize'] = {
   // Body
   'body-02': ['16px', '24px'],
   'body-long-02': ['16px', '28px'],
@@ -7,18 +9,17 @@ export const fontSize = {
   caption: ['12px', '18px'],
 
   // Title / Display
-  'display-05': ['40px', '52px'],
-  'display-04': ['36px', '46px'],
-  'display-03': ['32px', '42px'],
-  'display-02': ['28px', '38px'],
-  'display-01': ['24px', '34px'],
-  headline: ['20px', '28px'],
+  'display-05': ['40px', { lineHeight: '52px', fontWeight: 'bold' }],
+  'display-04': ['36px', { lineHeight: '46px', fontWeight: 'bold' }],
+  'display-03': ['32px', { lineHeight: '42px', fontWeight: 'bold' }],
+  'display-02': ['28px', { lineHeight: '38px', fontWeight: 'bold' }],
+  'display-01': ['24px', { lineHeight: '34px', fontWeight: 'bold' }],
+  headline: ['20px', { lineHeight: '28px', fontWeight: 'bold' }],
 
   // Subhead
-  'subhead-03': ['16px', '22px'],
-  'subhead-long-03': ['16px', '28px'],
-  'subhead-02': ['14px', '20px'],
-  'subhead-long-02': ['14px', '24px'],
-  'subhead-01': ['12px', '18px'],
-  'subhead-long-01': ['12px', '18px'],
+  'subhead-03': ['16px', { lineHeight: '22px', fontWeight: 'semibold' }],
+  'subhead-long-03': ['16px', { lineHeight: '28px', fontWeight: 'medium' }],
+  'subhead-02': ['14px', { lineHeight: '20px', fontWeight: 'semibold' }],
+  'subhead-long-02': ['14px', { lineHeight: '24px', fontWeight: 'medium' }],
+  'subhead-01': ['12px', { lineHeight: '18px', fontWeight: 'semibold' }],
 } as const
