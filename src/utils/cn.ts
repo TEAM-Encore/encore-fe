198633e5dcaf267497f308eefb5a0a4178477a1a
thyeone <thyeonee@gmail.com>
2025-08-20 16:51:46 +0900

    fix: 주석제거

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -3,8 +3,6 @@ import { fontSize } from '@/styles/fontSize'
 import { type ClassValue, clsx } from 'clsx'
 import { extendTailwindMerge } from 'tailwind-merge'
 
-// 중첩된 색상 키들을 평면화
-
 const twMerge = extendTailwindMerge({
   extend: {
     classGroups: {
