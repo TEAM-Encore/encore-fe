2bd981986f8bcf2d3af23a1cdbb6c5f12d72e9d1
TaeHyeon Kim <thyeonee@gmail.com>
2025-08-23 14:54:10 +0900

    feat: 후기옵션선택_가로형 (#25)
    * feat: 후기옵션선택_가로형
    
    * fix: Flex 컴포넌트 수정
    
    * fix: last 선택자 제거
    
    * feat: TextArea
    
    * fix: transiton 일단 제거
    
    * fix: 코드정리
    
    * fix: fontWeight 숫자로

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,35 +1,38 @@
-import { Avatar } from '@/components/Avatar'
+import { ReviewOptions } from '@/components/ReviewOptions'
+import { TextField } from '@/components/TextField'
 import { zodResolver } from '@hookform/resolvers/zod'
-import * as ImagePicker from 'expo-image-picker'
 import { useForm } from 'react-hook-form'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { z } from 'zod'
 
 const schema = z.object({
-  uri: z.string().optional(),
+  review: z.enum(['GOOD', 'NORMAL', 'BAD']),
 })
 
-type FormType = z.infer<typeof schema>
-
 export default function Index() {
-  const form = useForm<FormType>({
+  const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
+    defaultValues: {
+      review: 'GOOD',
+    },
   })
-
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-gray-12">
-      <Avatar
-        source={{ uri: form.watch('uri') }}
-        onUpload={async () => {
-          const result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ['images'],
-            selectionLimit: 1,
-          })
-
-          if (result.assets) {
-            form.setValue('uri', result.assets[0].uri)
-          }
+    <SafeAreaView className="flex-1 items-center justify-center bg-gray-12 px-5">
+      <ReviewOptions
+        value={form.watch('review')}
+        options={[
+          { label: '잘 들려요', value: 'GOOD' },
+          { label: '보통이에요', value: 'NORMAL' },
+          { label: '좋지 않아요', value: 'BAD' },
+        ]}
+        onSelect={(value) => {
+          form.setValue('review', value)
         }}
+      />
+      <TextField
+        as="textarea"
+        placeholder="리뷰를 입력해주세요"
+        className="mt-4"
       />
     </SafeAreaView>
   )
