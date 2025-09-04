d60e692ab951a20521c0e95f90f10a4d2a62f24a
bigcloud07 <dnsxo0712@gmail.com>
2025-09-04 16:26:48 +0900

    fix: 코드 리뷰 피드백 반영

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,30 +1,24 @@
-import { ReviewOptions } from '@/components/ReviewOptions'
-import { TextField } from '@/components/TextField'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import { useState } from 'react'
+import { Pressable, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { z } from 'zod'
-
-const schema = z.object({
-  review: z.enum(['GOOD', 'NORMAL', 'BAD']),
-})
+import { ReviewCard } from '@/components/ReviewCard'
+import posterplaceholder from '../../assets/images/poster-placeholder.png'
 
 export default function Index() {
   const [selectedId, setSelectedId] = useState<number | null>(null)
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#111' }}>
-      <Pressable style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} onPress={() => setSelectedId(null)}>
+      <Pressable
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        onPress={() => setSelectedId(null)}
+      >
         <View
           style={{
-
             width: 320,
             gap: 20,
           }}
         >
-
-        
-
           <ReviewCard
             active={selectedId === 1}
             onPress={() => setSelectedId(1)}
@@ -33,9 +27,7 @@ export default function Index() {
             summary={`넘버 퀄리티부터 배우합까지, 전반적으로 모두 만족스러웠던 공연.`}
             author="뮤사랑"
             likes={10}
-            
           />
-          
         </View>
       </Pressable>
     </SafeAreaView>
--- src/components/ReviewCard.tsx
+++ src/components/ReviewCard.tsx
@@ -1,89 +1,115 @@
+import React from 'react'
+import { Image, ImageSourcePropType, View } from 'react-native'
 import { Icon } from '@/components/common/icons/Icon'
 import { Col, Flex, Row } from '@/components/common/ui/Flex'
 import { Text } from '@/components/common/ui/Text'
 import { cn } from '@/utils/cn'
-import React from 'react'
-import { Image, View } from 'react-native'
 
 type ReviewCardProps = {
-    title: string
-    summary: string
-    author: string
-    likes: number
-    posterUrl: string
-    onPress?: () => void
-    className?: string
-    active?: boolean
+  title: string
+  summary: string
+  author: string
+  likes: number
+  posterUrl?: ImageSourcePropType | undefined
+  onPress?: () => void
+  className?: string
+  active?: boolean
 }
 
 export function ReviewCard({
-    title, summary, author, likes, posterUrl, onPress, className, active = false,
+  title,
+  summary,
+  author,
+  likes,
+  posterUrl,
+  onPress,
+  className,
+  active = false,
 }: ReviewCardProps) {
-    return (
-        <Flex
-            direction="row"
-            onPress={onPress}
-            className={cn(
-                'w-full h-[126px] py-[17px] px-[16px] rounded-[8px] bg-gray-11 flex-row',
-                {
-                    'bg-gray-03': active,
-                    'bg-gray-11': !active,
-                },
-                className,
-            )}
+  return (
+    <Flex
+      direction="row"
+      onPress={onPress}
+      className={cn(
+        'h-[126px] w-full flex-row rounded-[8px] bg-gray-11 px-[16px] py-[17px]',
+        {
+          'bg-gray-03': active,
+          'bg-gray-11': !active,
+        },
+        className,
+      )}
+    >
+      <View className="h-[92px] w-[66px] overflow-hidden rounded-[12px] bg-white/5">
+        {posterUrl ? (
+          <Image
+            source={posterUrl}
+            className="h-full w-full"
+            resizeMode="cover"
+          />
+        ) : null}
+      </View>
+
+      <Col className="ml-[16px] flex-1">
+        <Text
+          variant="subhead-03"
+          className={cn(active ? 'text-gray-12' : 'text-gray-01')}
+          numberOfLines={1}
         >
-            {/* 포스터 */}
-            <View className="w-[66px] h-[92px] rounded-[12px] overflow-hidden bg-white/5">
-                {posterUrl ? <Image source={posterUrl} className="w-full h-full" resizeMode="cover" /> : null}
-            </View>
+          {title}
+        </Text>
 
-            {/* 리뷰 정보 */}
-            <Col className="flex-1 ml-[16px]">
-                {/* 제목 */}
-                <Text variant="subhead-03" className={cn(active ? 'text-gray-12' : 'text-gray-01')} numberOfLines={1}>
-                    {title}
-                </Text>
+        <Text
+          variant="body-01"
+          className={cn('text-gray-07', 'mt-[6px]')}
+          numberOfLines={2}
+          ellipsizeMode="tail"
+        >
+          {summary}
+        </Text>
 
-                {/* 요약 */}
-                <Text
-                    variant="body-01"
-                    className={cn('text-gray-07', 'mt-[6px]')}
-                    numberOfLines={2}
-                    ellipsizeMode="tail"
-                >
-                    {summary}
-                </Text>
+        <Row className="mt-[8px] gap-x-[8px]" align="center">
+          <Row className="gap-x-[4px]" align="center">
+            <Icon
+              name="User"
+              size={12}
+              className={cn({
+                'text-gray-10': active,
+                'text-gray-01': !active,
+              })}
+            />
+            <Text
+              variant="caption"
+              className={cn({
+                'text-gray-10': active,
+                'text-gray-01': !active,
+              })}
+              numberOfLines={1}
+            >
+              {author}
+            </Text>
+          </Row>
 
-                {/* 작성자, like Count */}
-                <Row className="items-center gap-x-[8px] mt-[8px]">
-                    <Row className="items-center gap-x-[4px]">
-                        <Icon name="User" size={12} className={cn({
-                            'text-gray-10': active,
-                            'text-gray-01': !active,
-                        })} />
-                        <Text variant="caption" className={cn({
-                            'text-gray-10': active,
-                            'text-gray-01': !active,
-                        },)} numberOfLines={1}>
-                            {author}
-                        </Text>
-                    </Row>
-
-                    <Row className="items-center gap-x-[4px]">
-
-                        <Icon name="Like" size={12} className={cn({
-                            'text-gray-10': active,
-                            'text-gray-01': !active,
-                        })} />
-                        <Text variant="caption" className={cn({
-                            'text-gray-10': active,
-                            'text-gray-01': !active,
-                        })}>
-                            {likes}
-                        </Text>
-                    </Row>
-                </Row>
-            </Col>
-        </Flex>
-    )
+          <Row className="gap-x-[4px]" align="center">
+            <Icon
+              name="Like"
+              size={12}
+              className={cn({
+                'text-gray-10': active,
+                'text-gray-01': !active,
+              })}
+            />
+            <Text
+              variant="caption"
+              className={cn({
+                'text-gray-10': active,
+                'text-gray-01': !active,
+              })}
+            >
+              {likes}
+            </Text>
+          </Row>
+        </Row>
+      </Col>
+    </Flex>
+  )
 }
