dce9bda299386051a33579ec1a8e411ef9fc4e71
thyeone <thyeonee@gmail.com>
2025-09-28 10:39:30 +0900

    fix: font weight 버그 수정

--- src/components/common/ui/Text.tsx
+++ src/components/common/ui/Text.tsx
@@ -57,21 +57,12 @@ export const textVariants = cva('tracking-[-0.6px]', {
       'sub-black': 'text-sub-black',
       'sub-white': 'text-sub-white',
     },
-    weight: {
-      light: 'font-light',
-      regular: 'font-regular',
-      medium: 'font-medium',
-      semibold: 'font-semibold',
-      bold: 'font-bold',
-      extraBold: 'font-extraBold',
-    },
   },
 })
 
 export type TextProps = RNTextProps & {
   variant?: VariantProps<typeof textVariants>['variant']
   color?: ColorKeys
-  weight?: VariantProps<typeof textVariants>['weight']
   flex?: number
 }
 
@@ -81,7 +72,6 @@ export const Text = forwardRef<RNText, TextProps>(function Text(
     className,
     variant = 'body-01',
     color = 'gray-12',
-    weight = 'regular',
     flex,
     style,
     ...props
@@ -91,7 +81,7 @@ export const Text = forwardRef<RNText, TextProps>(function Text(
   return (
     <Animated.Text
       ref={ref}
-      className={cn(textVariants({ variant, color, weight }), className)}
+      className={cn(textVariants({ variant, color }), className)}
       style={[{ flex }, style]}
       {...props}
     >
