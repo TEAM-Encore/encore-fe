093f2692abe631e29c8b7ed96da849c3d1f95bb6
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-10-09 21:28:59 +0900

    fix: 로그인 버튼 수정

--- src/app/(auth)/login/components/LoginButton.tsx
+++ src/app/(auth)/login/components/LoginButton.tsx
@@ -1,7 +1,12 @@
-import { Button } from '@/components/Button'
 import { Icon } from '@/components/common/icons/Icon'
+import { Row } from '@/components/common/ui/Flex'
 import { Text } from '@/components/common/ui/Text'
 import { cn } from '@/utils/cn'
+
+const buttonColor = {
+  kakao: 'bg-[#FEE500]',
+  google: 'bg-white',
+}
 
 interface LoginButtonProps {
   type: 'Kakao' | 'Google'
@@ -9,23 +14,20 @@ interface LoginButtonProps {
 }
 
 export default function LoginButton({ type, onPress }: LoginButtonProps) {
-  const buttonColor = {
-    kakao: 'bg-[#FEE500]',
-    google: 'bg-white',
-  }
-
   return (
-    <Button
+    <Row
+      align="center"
+      justify="center"
       className={cn(
-        'relative h-[50px] rounded-[60px] px-4 active:bg-inherit active:opacity-90',
+        'relative h-[50px] rounded-[60px] px-4 active:opacity-90',
         buttonColor[type.toLowerCase() as keyof typeof buttonColor],
       )}
       onPress={onPress}
     >
+      <Icon name={type} size={18} className="absolute left-4" />
       <Text variant="body-02" className="text-gray-12">
         {type} 로그인
       </Text>
-      <Icon name={type} size={18} className="absolute left-4" />
-    </Button>
+    </Row>
   )
 }
