6ed089fd53be3d3db968d6c1b617de43467766b7
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-10-09 21:28:28 +0900

    fix: auth layout slot -> stack

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,5 +1,9 @@
-import { Slot, Stack } from 'expo-router'
+import { Stack } from 'expo-router'
 
 export default function Layout() {
-  return <Slot />
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="login" />
+    </Stack>
+  )
 }
