aa223d32d3b41ef8f15268616054002b01c89f27
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-10-12 10:34:05 +0900

    fix: 프로필 세팅 페이지 컴포넌트 수정

--- src/app/(auth)/login/profile-setup/index.tsx
+++ src/app/(auth)/login/profile-setup/index.tsx
@@ -1,19 +1,21 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { overlay } from 'overlay-kit'
-import { Controller, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 import { StatusBar } from 'react-native'
 import { Avatar } from '@/components/Avatar'
 import { Button } from '@/components/Button'
-import { Col, Flex, Row } from '@/components/common/ui/Flex'
+import { Col, Flex } from '@/components/common/ui/Flex'
 import { Screen } from '@/components/common/ui/Screen'
+import { Spacing } from '@/components/common/ui/Spacing'
 import { Text } from '@/components/common/ui/Text'
 import { Header } from '@/components/Header'
-import { TextField } from '@/components/TextField'
+import { FormTextField } from '@/components/TextField'
 import { GalleryBottomSheet } from './components/GalleryBottomSheet'
 import { type LoginFormType, loginSchema } from './schema'
 
 export default function ProfileSetup() {
   const form = useForm<LoginFormType>({
+    mode: 'onSubmit',
     resolver: zodResolver(loginSchema),
     defaultValues: {
       image: undefined,
@@ -33,10 +35,15 @@ export default function ProfileSetup() {
           <Header.Center>회원가입</Header.Center>
         </Header>
       }
-      fixedButton={<Button onPress={onSubmit}>시작하기</Button>}
+      fixedButton={
+        <Button disabled={!form.formState.isValid} onPress={onSubmit}>
+          시작하기
+        </Button>
+      }
     >
       <StatusBar barStyle="light-content" />
-      <Col className="mt-6">
+      <Spacing size={24} />
+      <Col>
         <Text variant="subhead-05" color="gray-01">
           프로필만 설정하면
         </Text>
@@ -45,38 +52,29 @@ export default function ProfileSetup() {
         </Text>
       </Col>
 
-      <Flex center className="mt-6">
+      <Spacing size={24} />
+      <Flex center>
         <Avatar
           onUpload={() => overlay.open((o) => <GalleryBottomSheet {...o} />)}
         />
       </Flex>
 
-      <Controller
+      <Spacing size={32} />
+      <FormTextField
         control={form.control}
         name="nickname"
-        render={({ field }) => (
-          <Col gap={10} className="mt-8">
-            <Row gap={16} className="relative w-full">
-              <TextField
-                style={{ paddingRight: 90 }}
-                placeholder="닉네임을 입력해주세요."
-                placeholderTextColor="#8B8B8B"
-                {...field}
-                onChangeText={field.onChange}
-                error={form.formState.errors.nickname?.message}
-                rightElement={() => (
-                  <Col
-                    align="center"
-                    justify="center"
-                    className="h-7 w-[64px] rounded-[4px] bg-primary-04"
-                  >
-                    <Text variant="caption" color="gray-12">
-                      중복 확인
-                    </Text>
-                  </Col>
-                )}
-              />
-            </Row>
+        style={{ paddingRight: 90 }}
+        placeholder="닉네임을 입력해주세요."
+        placeholderTextColor="#8B8B8B"
+        rightElement={() => (
+          <Col
+            align="center"
+            justify="center"
+            className="h-7 w-[64px] rounded-[4px] bg-primary-04"
+          >
+            <Text variant="caption" color="gray-12">
+              중복 확인
+            </Text>
           </Col>
         )}
       />
