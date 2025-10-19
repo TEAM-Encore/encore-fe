a2dddcd180bfe02f50d5eb5defd62560c4154434
chaeyun-sim <111689342+chaeyun-sim@users.noreply.github.com>
2025-10-19 21:18:58 +0900

    feat: 홈 페이지 퍼블리싱 (1.1, 1.2) (#50)
    * chore: Ticket, UserLinear, Edit, Feather 등 아이콘 생성
    
    * feat: 홈 페이지 퍼블리싱
    
    * feat: 앱바관련 빈 페이지 생성
    
    * fix: Flatlist에서 사용하는 키 변경 title -> id
    
    * fix: 탭 데이터 처리 수정 및 Row/Col props 간소화

---
+++ src/app/(auth)/search/index.tsx
@@ -0,0 +1,10 @@
+import { Screen } from '@/components/common/ui/Screen'
+import { Text } from '@/components/common/ui/Text'
+
+export default function Search() {
+  return (
+    <Screen>
+      <Text>Search</Text>
+    </Screen>
+  )
+}
