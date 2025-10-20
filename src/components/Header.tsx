d4f5e9b68acd145515a5c84e15cee81e04bcc488
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-10-20 23:05:42 +0900

    fix: Header 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 import { useRouter } from 'expo-router'
 import { View } from 'react-native'
+import { cn } from '@/utils/cn'
 import { Icon } from './common/icons/Icon'
 import { Flex, Row } from './common/ui/Flex'
 import { Text } from './common/ui/Text'
-import { cn } from '@/utils/cn'
 
 export function Header({
   children,
   progress,
   className,
-}: PropsWithStrictChildren<{ progress?: number, className?: string }>) {
+}: PropsWithStrictChildren<{ progress?: number; className?: string }>) {
   return (
-    <Row align="center" className={cn("relative h-[70px] px-5", className)}>
+    <Row align="center" className={cn('relative h-[70px] px-5', className)}>
       {children}
       {progress !== undefined && <Progress progress={progress} />}
     </Row>
@@ -37,9 +37,12 @@ function Left({ children }: PropsWithStrictChildren) {
   )
 }
 
-function Right({ children }: PropsWithStrictChildren) {
+function Right({
+  children,
+  className,
+}: PropsWithStrictChildren<{ className?: string }>) {
   return (
-    <Flex align="center" gap={12} className="z-10 ml-auto">
+    <Flex align="center" gap={12} className={cn('z-10 ml-auto', className)}>
       {children}
     </Flex>
   )
