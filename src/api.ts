75e45a26141e6db0ff09d5e0f0308608a23645aa
thyeone <thyeonee@gmail.com>
2025-10-27 16:24:49 +0900

    feat: 임시 fetchFn 추가

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,16 @@
 import { Api } from 'api'
 
+function customFetch(
+  input: RequestInfo | URL,
+  init?: RequestInit,
+): Promise<Response> {
+  return fetch(input, init)
+}
+
 /**
  * 토큰 로직 추가 필요
  */
-const api = <T>(fetchFunction = fetch): Api<T>['api'] => {
+const api = <T>(fetchFn: typeof fetch = customFetch): Api<T>['api'] => {
   const instance = new Api({
     baseUrl: process.env.EXPO_PUBLIC_API_HOST,
     baseApiParams: {
@@ -12,7 +19,7 @@ const api = <T>(fetchFunction = fetch): Api<T>['api'] => {
       cache: 'no-store',
     },
     customFetch: async (input: RequestInfo | URL, init?: RequestInit) => {
-      const response = await fetchFunction(input, init)
+      const response = await fetchFn(input, init)
 
       return response
     },
