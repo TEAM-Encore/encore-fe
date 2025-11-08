31330c1cb65ee6abf0834112a232c7cdbb6c67c2
Coa <coalee22@gmail.com>
2025-11-08 16:58:46 +0900

    2.1.1. 후기 작성할 티켓 선택 (#46)
    * settings
    
    * feat: import 추가, Screen에 fixedButton 있을 시 컨텐츠 패딩 추가
    
    * feat: 후기글 추가 - 2.1.1 후기 작성할 티켓 선택
    
    * fix: 피드백 반영
    
    * feat: Header의 뒤로가기 제거, 닫기 버튼 및 모달 추가
    
    * refactor: StepHeader
    
    * fix: 스타일

---
+++ src/components/StepIndicator.tsx
@@ -0,0 +1,30 @@
+import { Col } from '@/components/common/ui/Flex'
+import { Text } from '@/components/common/ui/Text'
+
+interface StepIndicatorProps {
+  currentStep: number
+  totalSteps: number
+  instruction: string
+  className?: string
+}
+
+export function StepIndicator({
+  currentStep,
+  totalSteps,
+  instruction,
+  className,
+}: StepIndicatorProps) {
+  return (
+    <Col className={className}>
+      {/* Step Number */}
+      <Text variant="body-01" className="mb-3 text-gray-07">
+        {currentStep}/{totalSteps}
+      </Text>
+
+      {/* Instruction */}
+      <Text variant="subhead-05" className="font-semibold text-gray-01">
+        {instruction}
+      </Text>
+    </Col>
+  )
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,6 @@
-import BottomSheet from '@gorhom/bottom-sheet'
+import BottomSheet from "@gorhom/bottom-sheet";
 
-export { BottomSheet as GorhomSheet }
+export { StepHeader } from "./StepHeader";
+export { StepIndicator } from "./StepIndicator";
+export { TicketBook } from "./TicketBook";
+export { BottomSheet as GorhomSheet };
