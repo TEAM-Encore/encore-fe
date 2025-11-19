58ce48d9a0cea87c9e70b0bb13d5f9aba29801f0
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-11-19 23:27:29 +0900

    chore: expo-secure-store 설치 및 storage 작성

---
+++ src/lib/storage.ts
@@ -0,0 +1,15 @@
+import * as SecureStore from 'expo-secure-store';
+
+type Key = 'accessToken' | 'refreshToken';
+
+export async function saveToken(key: Key, value: string) {
+  await SecureStore.setItemAsync(key, value);
+}
+
+export async function getToken(key: Key) {
+  return await SecureStore.getItemAsync(key);
+}
+
+export async function deleteToken(key: Key) {
+  await SecureStore.deleteItemAsync(key);
+}
