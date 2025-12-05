c58ba9396f402c262900b63f5eecd46c3003b6e9
thyeone <thyeonee@gmail.com>
2025-12-05 16:17:01 +0900

    fix: 옵션 제거

--- src/providers/react-query.provider.tsx
+++ src/providers/react-query.provider.tsx
@@ -2,18 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 
 export function ReactQueryProvider({ children }: PropsWithStrictChildren) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            staleTime: 0,
-            gcTime: 0,
-            retry: false,
-          },
-        },
-      }),
-  )
+  const [queryClient] = useState(() => new QueryClient())
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
