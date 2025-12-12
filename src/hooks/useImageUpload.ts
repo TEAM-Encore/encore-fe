31184a4018a1b008c013437e458d7d58ae84bd74
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-13 01:19:14 +0900

    feat: 이미지 생성 훅 예외처리 추가

--- src/hooks/useImageUpload.ts
+++ src/hooks/useImageUpload.ts
@@ -4,37 +4,41 @@ export const useImageUpload = () => {
 
 		const response = await fetch(`${process.env.EXPO_PUBLIC_API_HOST}/api/v1/image/presigned-url`, {
 			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				image_name: fileName,
-			}),
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ image_name: fileName }),
 		})
+
+		if (!response.ok) {
+			throw new Error('이미지 등록에 실패했습니다.')
+		}
+
 		const { file_path, upload_url } = await response.json()
 
 		const uploadResponse = await fetch(upload_url, {
-			body: blob,
 			method: 'PUT',
 			headers: {
 				'Content-Type': blob.type,
 			},
+			body: blob,
 		})
 
-		if (uploadResponse.ok) {
-			const getImageResponse = await fetch(`${process.env.EXPO_PUBLIC_API_HOST}/api/v1/image/view/presigned-url`, {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ file_path }),
-			})
-			const { url } = await getImageResponse.json()
-
-			return url
+		if (!uploadResponse.ok) {
+			throw new Error('이미지 등록에 실패했습니다.')
 		}
 
-		throw new Error('Failed to upload image')
+		const getImageResponse = await fetch(`${process.env.EXPO_PUBLIC_API_HOST}/api/v1/image/view/presigned-url`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ file_path }),
+		})
+
+		if (!getImageResponse.ok) {
+			throw new Error('이미지 등록에 실패했습니다.')
+		}
+
+		const { url } = await getImageResponse.json()
+
+		return url
 	}
 
 	return uploadImage
