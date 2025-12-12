1773ffb6b891b3cf68c014bc3f10af19a761019b
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-13 01:30:15 +0900

    feat: 토큰이 없을 경우 로그인 페이지로 이동

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,5 +1,19 @@
-import { Stack } from 'expo-router'
+import { getToken } from '@/lib/storage'
+import { router, Stack, usePathname } from 'expo-router'
+import { useEffect } from 'react'
 
 export default function Layout() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const checkAccess = async () => {
+      const preventNavigate = ['/login', '/login/profile-setup'];
+      const isLoggedIn = await getToken('accessToken')
+      if (!isLoggedIn && !preventNavigate.includes(pathname)) router.replace('/login')
+    }
+
+    checkAccess().catch(console.error)
+  }, [pathname])
+
   return <Stack screenOptions={{ headerShown: false }} />
 }
