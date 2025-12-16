139e6593a36fa89874bc3438689b49cee070f32a
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-16 17:27:26 +0900

    fix: 후기글 api 수정

--- src/app/_components/ReviewStep.tsx
+++ src/app/_components/ReviewStep.tsx
@@ -1,8 +1,10 @@
 import { useInfiniteQuery } from '@tanstack/react-query'
+import { useRouter } from 'expo-router'
 import { useEffect, useRef, useState } from 'react'
 import { ActivityIndicator, FlatList } from 'react-native'
 import { Spacing } from '@/components/common/ui/Spacing'
 import { ReviewCard } from '@/components/ReviewCard'
+import { useUser } from '@/providers/user.provider'
 import SortSelector from './SortSelector'
 
 const tabs = [
@@ -11,47 +13,43 @@ const tabs = [
 ]
 
 export default function ReviewStep() {
+  const router = useRouter()
   const flatListRef = useRef<FlatList>(null)
 
+  const user = useUser()
   const [sort, setSort] = useState<(typeof tabs)[number]['value']>(
     tabs[0].value,
   )
 
-  const {
-    data,
-    fetchNextPage,
-    hasNextPage,
-    isFetchingNextPage,
-    isLoading,
-    error,
-  } = useInfiniteQuery({
-    queryKey: ['reviews', sort],
-    queryFn: async ({ pageParam }) => {
-      const params = new URLSearchParams({
-        page: '0',
-        size: '3',
-        sort: sort,
-      })
-      if (pageParam != null) params.set('cursor', String(pageParam))
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading } =
+    useInfiniteQuery({
+      queryKey: ['reviews', sort],
+      queryFn: async ({ pageParam }) => {
+        const params = new URLSearchParams({
+          cursor: pageParam?.toString() ?? '',
+          'pageable.page': '0',
+          'pageable.size': '3',
+          'pageable.sort': sort,
+          userId: String(user?.id),
+        })
+        if (pageParam != null) params.set('cursor', String(pageParam))
 
-      const response = await fetch(
-        `${process.env.EXPO_PUBLIC_API_HOST}/api/mvp/review/list?${params}`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
+        const response = await fetch(
+          `${process.env.EXPO_PUBLIC_API_HOST}/api/mvp/review/list?${params}`,
+          {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+            },
           },
-        },
-      )
-      return response.json().then((data) => data.data)
-    },
-    initialPageParam: undefined as number | undefined,
-    getNextPageParam: (lastPage) => {
-      return lastPage?.nextCursor ?? undefined
-    },
-  })
-
-  console.log(error)
+        )
+        return response.json().then((data) => data.data)
+      },
+      initialPageParam: undefined as number | undefined,
+      getNextPageParam: (lastPage) => {
+        return lastPage?.nextCursor ?? undefined
+      },
+    })
 
   const reviews = data?.pages.flatMap((page) => page?.content ?? []) ?? []
 
@@ -74,7 +72,7 @@ export default function ReviewStep() {
             summary={item.content ?? ''}
             author={item.nickname ?? ''}
             likes={item.like_count ?? 0}
-            // onPress={() => router.push(`/review-detail/${item.review_id}`)}
+            onPress={() => router.push(`/review-detail/${item.review_id}`)}
           />
         )}
         onEndReached={() => {
