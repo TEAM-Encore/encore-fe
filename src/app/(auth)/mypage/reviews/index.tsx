3fd4aeaa0eef1b3bcdf6166f122a3849ef221877
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2025-12-30 20:54:41 +0900

    feaet: 내 작성 글 목록 api 연결

--- src/app/(auth)/mypage/reviews/index.tsx
+++ src/app/(auth)/mypage/reviews/index.tsx
@@ -1,15 +1,39 @@
+import type {
+  GetReviewListData,
+  GetReviewListParams,
+  ReviewGetListRes,
+} from 'api'
 import { LinearGradient } from 'expo-linear-gradient'
-import { FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+import { ActivityIndicator, FlatList } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { REVIEW_MOCK } from '@/app/_components/ReviewStep'
+import { api } from '@/api'
 import { Screen } from '@/components/common/ui/Screen'
 import { Header } from '@/components/Header'
 import { ReviewCard } from '@/components/ReviewCard'
-import { useRouter } from 'expo-router'
+import { useInfiniteList } from '@/hooks/useInfiniteList'
+import { useUser } from '@/providers/user.provider'
 
 export default function Reviews() {
   const insets = useSafeAreaInsets()
   const router = useRouter()
+
+  const user = useUser()
+
+  const {
+    items: reviews,
+    loadMore,
+    ...queryProps
+  } = useInfiniteList<GetReviewListParams, ReviewGetListRes, GetReviewListData>(
+    {
+      queryKey: ['reviews'],
+      fn: api().getReviewList,
+      params: {
+        pageable: { page: 0, size: 3, sort: [] },
+        userId: user?.id ?? 0,
+      },
+    },
+  )
 
   return (
     <Screen
@@ -21,19 +45,38 @@ export default function Reviews() {
       }
     >
       <FlatList
-        data={REVIEW_MOCK}
+        data={reviews}
         contentContainerClassName="gap-5 py-6"
         showsVerticalScrollIndicator={false}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item, index) =>
+          item.review_id?.toString() ?? index.toString()
+        }
         renderItem={({ item }) => (
           <ReviewCard
-            {...item}
+            title={item.title ?? ''}
+            summary={item.content ?? ''}
+            author={item.nickname ?? ''}
+            likes={item.like_count ?? 0}
             hideImage
             onPress={() =>
-              router.push(`/review-detail/${item.id}?from=mypage-reviews`)
+              router.push(
+                `/review-detail/${item.review_id}?from=mypage-reviews`,
+              )
             }
           />
         )}
+        onEndReached={loadMore}
+        onEndReachedThreshold={0.5}
+        ListFooterComponent={
+          queryProps.isFetchingNextPage ? (
+            <ActivityIndicator style={{ padding: 20 }} />
+          ) : null
+        }
+        ListEmptyComponent={
+          queryProps.isLoading ? (
+            <ActivityIndicator style={{ padding: 20 }} />
+          ) : null
+        }
       />
       <LinearGradient
         colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0.1)', '#000000']}
