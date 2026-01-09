ba0e12a9da9591eeb0282b5d982890ca5901f426
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-09 18:28:21 +0900

    fix: queryClient 분리

--- src/providers/react-query.provider.tsx
+++ src/providers/react-query.provider.tsx
@@ -1,19 +1,7 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { useState } from 'react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/query-client'
 
 export function ReactQueryProvider({ children }: PropsWithStrictChildren) {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            retry: 2,
-            staleTime: 1000 * 60 * 5, // 5분
-          },
-        },
-      }),
-  )
-
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   )
