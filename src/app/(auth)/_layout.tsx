d468aaa2675b9b06b26d974644fd63cfbffe2eba
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-01-09 19:08:43 +0900

    feat: 토큰 만료 시 로그인 페이지로 리다이렉트 추가

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -4,9 +4,8 @@ import { useUser } from '@/providers/user.provider'
 export default function AuthLayout() {
   const user = useUser()
 
-  if (!user) {
+  if (!user || (user.exp && user.exp < Date.now() / 1000))
     return <Redirect href="/login" />
-  }
 
   return <Stack screenOptions={{ headerShown: false }} />
 }
