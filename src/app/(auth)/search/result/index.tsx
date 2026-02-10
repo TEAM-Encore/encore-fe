c4ffe18e39fc4f6ca57d172bd5de125e168364e6
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-02-10 21:14:42 +0900

    fix: 검색 결과 페이지에서 search input focus 시 검색 기록 표시

--- src/app/(auth)/search/result/index.tsx
+++ src/app/(auth)/search/result/index.tsx
@@ -1,18 +1,25 @@
-import { router, useLocalSearchParams } from 'expo-router'
-import { useState } from 'react'
+import { useMutation, useQuery } from '@tanstack/react-query'
+import { router, useFocusEffect, useLocalSearchParams } from 'expo-router'
+import { useCallback, useState } from 'react'
 import { ActivityIndicator, FlatList } from 'react-native'
 import { api } from '@/api'
-import { Col, Flex } from '@/components/common/ui/Flex'
+import { searchKeys } from '@/apis/search/keys'
+import { searchMutations } from '@/apis/search/mutations'
+import { searchQueries } from '@/apis/search/queries'
+import { Icon } from '@/components/common/icons/Icon'
+import { Col, Flex, Row } from '@/components/common/ui/Flex'
 import { Screen } from '@/components/common/ui/Screen'
 import { Text } from '@/components/common/ui/Text'
 import { Header } from '@/components/Header'
 import { ReviewCard } from '@/components/ReviewCard'
 import { Search } from '@/components/search/Search'
 import { useInfiniteList } from '@/hooks/useInfiniteList'
+import { queryClient } from '@/lib/query-client'
 
 export default function SearchResult() {
   const { q } = useLocalSearchParams<{ q: string }>()
   const [searchValue, setSearchValue] = useState(q || '')
+  const [isFocusing, setIsFocusing] = useState(false)
 
   const handleSearch = () => {
     if (searchValue.trim()) {
@@ -33,6 +40,19 @@ export default function SearchResult() {
     enabled: !!searchValue,
   })
 
+  const { data: recentKeywords, refetch } = useQuery(
+    searchQueries.getRecentSearchLogs(),
+  )
+
+  useFocusEffect(
+    useCallback(() => {
+      refetch()
+    }, [refetch]),
+  )
+  const { mutate: deleteRecentSearchLog } = useMutation(
+    searchMutations.deleteRecentSearchLog(),
+  )
+
   return (
     <Screen
       className="!px-0"
@@ -48,11 +68,54 @@ export default function SearchResult() {
               onDelete={() => setSearchValue('')}
               className="placeholder:!text-gray-07 !py-[9px] h-[36px] w-full text-[14px]"
               onSubmitEditing={handleSearch}
+              onFocus={() => setIsFocusing(true)}
             />
           </Header.Right>
         </Header>
       }
     >
+      {isFocusing && (
+        <Col className="h-full w-full">
+          <FlatList
+            data={recentKeywords}
+            renderItem={({ item }) => (
+              <Row
+                key={item.name}
+                align="center"
+                justify="space-between"
+                className="border-b border-b-gray-09 px-5 py-[17px]"
+                onPress={() => {
+                  setSearchValue(item.name ?? '')
+                  router.replace(
+                    `/search/result?q=${encodeURIComponent(item.name ?? '')}`,
+                  )
+                }}
+              >
+                <Text color="gray-01" className="font-normal text-[16px]">
+                  {item.name}
+                </Text>
+                <Icon
+                  name="Close"
+                  size={24}
+                  color="#FBFBFB"
+                  onPress={() =>
+                    deleteRecentSearchLog(
+                      { name: item.name ?? '' },
+                      {
+                        onSuccess: () => {
+                          queryClient.invalidateQueries({
+                            queryKey: searchKeys.all,
+                          })
+                        },
+                      },
+                    )
+                  }
+                />
+              </Row>
+            )}
+          />
+        </Col>
+      )}
       {!queryProps.isLoading && reviews.length === 0 && (
         <Flex align="center" justify="center" className="h-full w-full">
           <Text variant="body-01" className="text-gray-06">
