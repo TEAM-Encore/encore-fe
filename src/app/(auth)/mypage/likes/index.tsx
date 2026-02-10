7637a94777d1fc0a78d127d450df10b8cfac3e59
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2026-02-10 12:06:06 +0900

    feat: 좋아요 목록 및 작성 리뷰 목록에 isLoading 처리, empty 처리 추가

--- src/app/(auth)/mypage/likes/index.tsx
+++ src/app/(auth)/mypage/likes/index.tsx
@@ -2,7 +2,9 @@ import { LinearGradient } from 'expo-linear-gradient'
 import { ActivityIndicator, FlatList } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { api } from '@/api'
+import { Flex } from '@/components/common/ui/Flex'
 import { Screen } from '@/components/common/ui/Screen'
+import { Text } from '@/components/common/ui/Text'
 import { Header } from '@/components/Header'
 import { ReviewCard } from '@/components/ReviewCard'
 import { useInfiniteList } from '@/hooks/useInfiniteList'
@@ -13,6 +15,7 @@ export default function Reviews() {
   const {
     rows: likes,
     fetchNextPage,
+    isLoading,
     ...queryProps
   } = useInfiniteList({
     queryKey: 'reviews',
@@ -32,41 +35,56 @@ export default function Reviews() {
         </Header>
       }
     >
-      <FlatList
-        data={likes ?? []}
-        contentContainerClassName="gap-5 py-6"
-        showsVerticalScrollIndicator={false}
-        keyExtractor={(item) => item?.review_id?.toString() ?? ''}
-        renderItem={({ item }) => (
-          <ReviewCard
-            title={item.title ?? ''}
-            summary={item.content ?? ''}
-            author={item.nickname ?? ''}
-            likes={item.like_count ?? 0}
-            hideImage
+      {isLoading && (
+        <Flex align="center" justify="center" className="h-full w-full">
+          <ActivityIndicator />
+        </Flex>
+      )}
+      {likes.length === 0 && !isLoading ? (
+        <Flex align="center" justify="center" className="h-full w-full">
+          <Text variant="body-01" className="text-gray-06">
+            좋아요 목록이 없습니다.
+          </Text>
+        </Flex>
+      ) : (
+        <>
+          <FlatList
+            data={likes ?? []}
+            contentContainerClassName="gap-5 py-6"
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item) => item?.review_id?.toString() ?? ''}
+            renderItem={({ item }) => (
+              <ReviewCard
+                title={item.title ?? ''}
+                summary={item.content ?? ''}
+                author={item.nickname ?? ''}
+                likes={item.like_count ?? 0}
+                hideImage
+              />
+            )}
+            onEndReached={fetchNextPage}
+            onEndReachedThreshold={0.5}
+            ListFooterComponent={
+              queryProps.isFetchingNextPage ? (
+                <ActivityIndicator style={{ padding: 20 }} />
+              ) : null
+            }
           />
-        )}
-        onEndReached={fetchNextPage}
-        onEndReachedThreshold={0.5}
-        ListFooterComponent={
-          queryProps.isFetchingNextPage ? (
-            <ActivityIndicator style={{ padding: 20 }} />
-          ) : null
-        }
-      />
-      <LinearGradient
-        colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0.1)', '#000000']}
-        locations={[0, 0.5, 1]}
-        style={{
-          position: 'absolute',
-          right: 0,
-          bottom: insets.bottom - 70,
-          left: 0,
-          height: 200,
-          zIndex: 40,
-          pointerEvents: 'none',
-        }}
-      />
+          <LinearGradient
+            colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0.1)', '#000000']}
+            locations={[0, 0.5, 1]}
+            style={{
+              position: 'absolute',
+              right: 0,
+              bottom: insets.bottom - 70,
+              left: 0,
+              height: 200,
+              zIndex: 40,
+              pointerEvents: 'none',
+            }}
+          />
+        </>
+      )}
     </Screen>
   )
 }
--- src/app/(auth)/mypage/reviews/index.tsx
+++ src/app/(auth)/mypage/reviews/index.tsx
@@ -3,7 +3,9 @@ import { useRouter } from 'expo-router'
 import { ActivityIndicator, FlatList } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { api } from '@/api'
+import { Flex } from '@/components/common/ui/Flex'
 import { Screen } from '@/components/common/ui/Screen'
+import { Text } from '@/components/common/ui/Text'
 import { Header } from '@/components/Header'
 import { ReviewCard } from '@/components/ReviewCard'
 import { useInfiniteList } from '@/hooks/useInfiniteList'
@@ -15,6 +17,7 @@ export default function Reviews() {
   const {
     rows: reviews,
     fetchNextPage,
+    isLoading,
     ...queryProps
   } = useInfiniteList({
     queryKey: 'reviews',
@@ -33,51 +36,61 @@ export default function Reviews() {
         </Header>
       }
     >
-      <FlatList
-        data={reviews ?? []}
-        contentContainerClassName="gap-5 py-6"
-        showsVerticalScrollIndicator={false}
-        keyExtractor={(item) => item?.review_id?.toString() ?? ''}
-        renderItem={({ item }) => (
-          <ReviewCard
-            title={item.title ?? ''}
-            summary={item.content ?? ''}
-            author={item.nickname ?? ''}
-            likes={item.like_count ?? 0}
-            hideImage
-            onPress={() =>
-              router.push(
-                `/review-detail/${item.review_id}?from=mypage-reviews`,
-              )
+      {isLoading && (
+        <Flex align="center" justify="center" className="h-full w-full">
+          <ActivityIndicator />
+        </Flex>
+      )}
+      {reviews.length === 0 && !isLoading ? (
+        <Flex align="center" justify="center" className="h-full w-full">
+          <Text variant="body-01" className="text-gray-06">
+            좋아요 목록이 없습니다.
+          </Text>
+        </Flex>
+      ) : (
+        <>
+          <FlatList
+            data={reviews ?? []}
+            contentContainerClassName="gap-5 py-6"
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item) => item?.review_id?.toString() ?? ''}
+            renderItem={({ item }) => (
+              <ReviewCard
+                title={item.title ?? ''}
+                summary={item.content ?? ''}
+                author={item.nickname ?? ''}
+                likes={item.like_count ?? 0}
+                hideImage
+                onPress={() =>
+                  router.push(
+                    `/review-detail/${item.review_id}?from=mypage-reviews`,
+                  )
+                }
+              />
+            )}
+            onEndReached={fetchNextPage}
+            onEndReachedThreshold={0.5}
+            ListFooterComponent={
+              queryProps.isFetchingNextPage ? (
+                <ActivityIndicator style={{ padding: 20 }} />
+              ) : null
             }
           />
-        )}
-        onEndReached={fetchNextPage}
-        onEndReachedThreshold={0.5}
-        ListFooterComponent={
-          queryProps.isFetchingNextPage ? (
-            <ActivityIndicator style={{ padding: 20 }} />
-          ) : null
-        }
-        ListEmptyComponent={
-          queryProps.isLoading ? (
-            <ActivityIndicator style={{ padding: 20 }} />
-          ) : null
-        }
-      />
-      <LinearGradient
-        colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0.1)', '#000000']}
-        locations={[0, 0.5, 1]}
-        style={{
-          position: 'absolute',
-          right: 0,
-          bottom: insets.bottom - 70,
-          left: 0,
-          height: 200,
-          zIndex: 40,
-          pointerEvents: 'none',
-        }}
-      />
+          <LinearGradient
+            colors={['rgba(0, 0, 0, 0)', 'rgba(0, 0, 0, 0.1)', '#000000']}
+            locations={[0, 0.5, 1]}
+            style={{
+              position: 'absolute',
+              right: 0,
+              bottom: insets.bottom - 70,
+              left: 0,
+              height: 200,
+              zIndex: 40,
+              pointerEvents: 'none',
+            }}
+          />
+        </>
+      )}
     </Screen>
   )
 }
