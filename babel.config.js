aaa6be3be9b4806d09ad28f6ed4a73f0fa488130
Teo <thyeonee@gmail.com>
2026-02-15 22:12:11 +0900

    chore: eas 설정중

--- babel.config.js
+++ babel.config.js
@@ -5,5 +5,19 @@ module.exports = (api) => {
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel',
     ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+            '@/assets': './assets',
+            '@/public': './public',
+            api: './codegen/__generated__/api_sdk',
+          },
+        },
+      ],
+    ],
   }
 }
